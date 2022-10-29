cfd3337bc00ea9ed19e29bfad0ba88e8e85979f5
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-29 23:28:24 +0530

    Ternary Operator

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 5;
+        int min = (a < b) ? a : b;
+        System.out.println(min);
+    }
+}
