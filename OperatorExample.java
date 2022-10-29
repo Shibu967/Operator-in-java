b3c39c0ee90c43b31ac0ad4816098333b99452fd
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-29 22:59:01 +0530

    Java Arithmetic Operator Example: Expression

---
+++ OperatorExample.java
@@ -0,0 +1,5 @@
+public class OperatorExample {
+    public static void main(String args[]) {
+        System.out.println(10 * 10 / 5 + 3 - 1 * 4 / 2);
+    }
+}// Java Arithmetic Operator Example: Expression
