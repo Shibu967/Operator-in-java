b7f5a113a95930f41705f471a89020f995f29d3a
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-29 23:24:59 +0530

    AssignmentOperatorExample4

---
+++ AssignmentOperatorExample4.java
@@ -0,0 +1,8 @@
+public class AssignmentOperatorExample4 {
+    public static void main(String args[]) {
+        short a = 10;
+        short b = 10;
+        a = (short) (a + b);// 20 which is int now converted to short
+        System.out.println(a);
+    }
+}
